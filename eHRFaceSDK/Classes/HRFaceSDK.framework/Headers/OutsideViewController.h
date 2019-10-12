abecc3269616960abefab4b834b861aa25ca0fef
lyt <24290265@qq.com>
2019-10-12 17:59:00 +0800

    修复先后顺序问题

--- eHRFaceSDK/Classes/HRFaceSDK.framework/Headers/OutsideViewController.h
+++ eHRFaceSDK/Classes/HRFaceSDK.framework/Headers/OutsideViewController.h
@@ -26,6 +26,8 @@ typedef void (^GoBackData)(NSString *statue);
 @property (nonatomic, copy) NSString *business_type; //业务类型
 @property (nonatomic, assign) NSInteger userType;//用户类型：0-主借款人，1-共还人
 @property (nonatomic, copy) NSString * esbUrl;
+@property (nonatomic, copy) NSString * url;
+
 @property (nonatomic, assign) BOOL IsIndependent; //是否是独立面签;
 @property (nonatomic, assign) BOOL IsOnline;
 @property (nonatomic, strong) NSDictionary * Nimdata;
@@ -38,9 +40,17 @@ typedef void (^GoBackData)(NSString *statue);
 @property (nonatomic, copy) NSString *actualLoanAmount;//贷款金额
 @property (nonatomic, copy) NSString *instalmentNum;//贷款期限
 
+
+@property (nonatomic, copy) NSString *firstMoney; //首付款 //选传
+@property (nonatomic, copy) NSString *installmentMoney; //分期金额 //选传
+@property (nonatomic, copy) NSString *serviceFee; //按揭服务费 //选传
+
 @property (nonatomic, assign) BOOL isBack;
 @property (nonatomic, copy) GoBackData backData;
 @property (nonatomic, weak) id <OutsideViewControllerDelegate> delegate;
+
+
+
 
 @end
 
